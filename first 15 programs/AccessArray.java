08897c93f6f464a40f218b251ae3bd6c4b4f8ffc
Arathijayaprakash <arathiparambil@gmail.com>
2021-05-26 13:11:47 +0530

    10th pgm

---
+++ first 15 programs/AccessArray.java
@@ -0,0 +1,9 @@
+public class AccessArray{
+	public static void main(String ar[]){
+		int[] array={12,4,5,2};
+		System.out.println("First element:"+array[0]);
+		System.out.println("Second element:"+array[1]);	
+		System.out.println("Third element:"+array[2]);	
+		System.out.println("Fourth element:"+array[3]);	
+	}
+}
