a9f2af65e6f3f664563d19a1aa91175d6cc39b09
Arathijayaprakash <arathiparambil@gmail.com>
2021-05-26 13:19:55 +0530

    11th pgm

---
+++ first 15 programs/ArrayusingLoop.java
@@ -0,0 +1,8 @@
+public class ArrayusingLoop{
+	public static void main(String ar[]){
+		String[] array={"apple","banana","mango","orange"};
+		for(int i=0;i<array.length;i++){
+			System.out.println(array[i]);
+		}
+	}
+}
