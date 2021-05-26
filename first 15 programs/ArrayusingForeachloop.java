e4915c302af8e776c747645c5931e5d39f04234f
Arathijayaprakash <arathiparambil@gmail.com>
2021-05-26 13:26:57 +0530

    12th pgm

---
+++ first 15 programs/ArrayusingForeachloop.java
@@ -0,0 +1,8 @@
+public class ArrayusingForeachloop{
+	public static void main(String ar[]){
+		String[] array={"apple","banana","mango","orange"};
+		for(String i:array){
+			System.out.println(i);
+		}
+	}
+}
