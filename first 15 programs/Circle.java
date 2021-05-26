59624a46f667c4195ac364ad8fec8b5e0d908e2b
Arathijayaprakash <arathiparambil@gmail.com>
2021-05-26 11:39:18 +0530

    4programs

---
+++ first 15 programs/Circle.java
@@ -0,0 +1,10 @@
+public class Circle{
+	public static void main(String ar[]){
+		double r=7.5,a,p;
+		a=3.14*7.5*7.5;
+		p=2*3.14*7.5;
+		System.out.println("Perimeter is : "+p);
+		System.out.println("Area is : "+a);
+		
+	}
+}
---
+++ first 15 programs/Sum.java
@@ -0,0 +1,8 @@
+public class Sum{
+	public static void main(String ar[]){
+		int a=3,b=2,c;
+		c=a+b;
+		System.out.println("Sum : "+c);
+		
+	}
+}
