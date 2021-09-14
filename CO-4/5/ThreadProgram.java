3b1d325c1d9ad80401095ddae699db61e99acac7
Arathijayaprakash <arathiparambil@gmail.com>
2021-09-14 12:23:33 +0530

    ...

--- CO-4/5/ThreadProgram.java
+++ CO-4/5/ThreadProgram.java
@@ -21,24 +21,20 @@ class Numbers{
 		System.out.println(2);
 		int num=3;
 		int status=1;
-		for ( int i = 2 ; i <=n ;  )
-      {
-         for ( int j = 2 ; j <= Math.sqrt(num) ; j++ )
-         {
-            if ( num%j == 0 )
-            {
-               status = 0;
-               break;
-            }
-         }
-         if ( status != 0 )
-         {
-            System.out.println(num);
-            i++;
-         }
-         status = 1;
-         num++;
-      }         
+		for(int i=2;i<=n;){
+			for(int j=2;j<=Math.sqrt(num);j++){
+				if(num%j==0){
+					status = 0;
+               				break;
+            				}
+         			}
+         		if(status!=0){
+				System.out.println(num);
+            			i++;
+         			}
+         		status=1;
+         		num++;
+      		}         
 	}
 
 }
